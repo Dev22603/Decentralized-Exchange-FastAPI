bd4d9094346865acc8f5ec670b3ac7a45f54fffe
Dev Bachani <dev22603@gmail.com>
2025-08-09 12:01:20 +0530

    ui changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "http://localhost:8000/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ export const getEthAmount = async (amount) => {
         headers: {
           'Content-Type': 'application/json',
         },
-        withCredentials: true, // Include cookies for session-based auth
+        withCredentials: true,
       });
       
       return response.data;
